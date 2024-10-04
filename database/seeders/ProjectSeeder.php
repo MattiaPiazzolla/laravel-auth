4436a8ceb1d0f10d9c7ff2e9b809841d7de77061
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-10-04 18:47:52 +0200

    [UPD] defined main structure of the layout

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -4,6 +4,12 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+// per lo slug aggiungo questo
+use Illuminate\Support\Str;
+
+use App\Models\Project;
 
 class ProjectSeeder extends Seeder
 {
@@ -12,8 +18,17 @@ class ProjectSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for($i = 0; $i<10; $i++ )   {
+            $project = new Project();
+            $project->name = $faker->sentence(4);
+            $project->summary = $faker->paragraphs(3, true);
+
+            $project->slug = Str::slug($project->name, '-');
+
+            $project->save();
+
+        }
     }
 }
