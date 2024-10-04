c05c65a8415c21959e379c5094ea77e9a6600f70
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-10-04 16:47:25 +0200

    [UPD] defined first migration

--- database/migrations/2024_10_04_143558_create_projects_table.php
+++ database/migrations/2024_10_04_143558_create_projects_table.php
@@ -15,6 +15,11 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+
+            $table->string('name',100);
+            $table->string('slug');
+            $table->text('summary')->nullable();
+
             $table->timestamps();
         });
     }
