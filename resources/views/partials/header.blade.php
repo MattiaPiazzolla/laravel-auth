760951c94e6ca1dc533ab04a34dfee49bcdc2401
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-10-08 15:39:13 +0200

    [UPD]change filesystem from local to public

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -16,7 +16,6 @@
 
                 <!-- Right Side Of Navbar -->
                 <ul class="navbar-nav ml-auto">
-                    <!-- Authentication Links -->
                     @guest
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
